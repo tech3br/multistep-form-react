b025c1ccafc90e352a91e4eb4dca73c064cfa89c
Daniel Filgueira <dfsdireito95@gmail.com>
2021-10-09 20:00:33 -0300

    feat(step one): add theme to step 1

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,7 +1,24 @@
 import * as C from "./styles";
+import { Theme } from "../../components/Theme";
 
 export const FormStep1 = () => {
+  const handleNextStep = () => {};
   return (
-    <C.Container>Passo 1</C.Container>
+    <Theme>
+      <C.Container>
+        <p>Passo 1/3</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preencha o campo abaixo com seu nome completo</p>
+
+        <hr />
+
+        <label>
+          Seu nome completo
+          <input type="text" autoFocus />
+        </label>
+
+        <button onClick={handleNextStep}>Próximo</button>
+      </C.Container>
+    </Theme>
   );
 };
