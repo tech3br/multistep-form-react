74235a9f986cfc22fa2088329a11fcff7fd41329
Daniel Filgueira <dfsdireito95@gmail.com>
2021-10-09 18:38:33 -0300

    feat(app): add form context around of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Router } from "./router";
-
+import { FormProvider } from "./contexts/FormContext";
 const App = () => {
-  return <Router />;
+  return (
+    <FormProvider>
+      <Router />
+    </FormProvider>
+  );
 };
 
 export default App;
