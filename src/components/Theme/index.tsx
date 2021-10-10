6e08b68ff20e704227d2559714873117cdffcec2
Daniel Filgueira <dfsdireito95@gmail.com>
2021-10-10 02:08:51 -0300

    feat(theme): add sidebar item into theme component

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import { Header } from "../Header";
 import * as C from "./styles";
-
+import { SideBarItem } from "../SideBarItem";
 type ThemeProps = { children: ReactNode };
 
 export const Theme = ({ children }: ThemeProps) => {
@@ -11,7 +11,26 @@ export const Theme = ({ children }: ThemeProps) => {
         <Header />
 
         <C.Steps>
-          <C.Sidebar>...</C.Sidebar>
+          <C.Sidebar>
+            <SideBarItem
+              title="Pessoal"
+              description="Se identifique"
+              icon="profile"
+              path="/"
+            />
+            <SideBarItem
+              title="Profissional"
+              description="Seu nível"
+              icon="book"
+              path="/step2"
+            />
+            <SideBarItem
+              title="Contatos"
+              description="Como te achar"
+              icon="mail"
+              path="/step3"
+            />
+          </C.Sidebar>
           <C.Page>{children}</C.Page>
         </C.Steps>
       </C.Area>
