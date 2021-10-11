161f82eb00b33728ba9d6d336e2ec97c29974eba
Daniel Filgueira <dfsdireito95@gmail.com>
2021-10-10 22:34:46 -0300

    refactor(theme): refactoring theme logic

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -2,9 +2,11 @@ import { ReactNode } from "react";
 import { Header } from "../Header";
 import * as C from "./styles";
 import { SideBarItem } from "../SideBarItem";
+import { useForm } from "../../contexts/FormContext";
 type ThemeProps = { children: ReactNode };
 
 export const Theme = ({ children }: ThemeProps) => {
+  const { state } = useForm();
   return (
     <C.Container>
       <C.Area>
@@ -17,18 +19,31 @@ export const Theme = ({ children }: ThemeProps) => {
               description="Se identifique"
               icon="profile"
               path="/"
+              selected={state.currentStep === 1}
             />
+
             <SideBarItem
               title="Profissional"
               description="Seu nível"
               icon="book"
               path="/step2"
+              selected={state.currentStep === 2}
             />
+
             <SideBarItem
               title="Contatos"
               description="Como te achar"
               icon="mail"
               path="/step3"
+              selected={state.currentStep === 3}
+            />
+
+            <SideBarItem
+              title="Resumo"
+              description="Resumo das suas informações"
+              icon="list"
+              path="/resume"
+              selected={state.currentStep === 4}
             />
           </C.Sidebar>
           <C.Page>{children}</C.Page>
